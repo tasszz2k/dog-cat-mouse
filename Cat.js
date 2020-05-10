2849b3fdf9072eb597e41d8fce147789469c5f33
TASS <tasszz2k@gmail.com>
2020-05-10 14:58:59 +0700

    update: update file cat.js

--- Cat.js
+++ Cat.js
@@ -1,5 +1,9 @@
 function name(params) {
     console.log('abs');
     console.log('2222222');
-    
+    for (let index = 0; index < 10; index++) {
+      console.log(index);
+      
+        
+    }
 }
